c287c655ccb8651f2a72310bfdea2b363e70c0bc
edw-perzd <legna246.dav@gmail.com>
2023-06-09 16:20:20 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+#Python es No tipado
+
+name = "Eduardo"
+last_name = "Perez"
+age = 18
+height = 1.65
+is_student = True
+groups = ["3C", "Guitarra"]
+
+#function type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
