1ff09cebb751b27427858d813be967db455585ed
edw-perzd <legna246.dav@gmail.com>
2023-06-09 16:52:03 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,20 @@
+# operadores matemáticos
+# + - * %
+# / División float
+# // División entera
+# ** Potencia
+
+print(12 + 12)
+print(12 - 12)
+print(12 * 12)
+print(12 % 12)
+print(10 / 3)
+print(10 // 3)
+print(2 ** 16)
+
+# Operadores para strings
+# + para concatenar
+# * para replicar
+print("_______" * 4)
+print("Hola " + "Mundo!")
+print("Hola " * 3)
